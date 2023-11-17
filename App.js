36a157380baa10955f4e8077e6aabcdc0207c9ef
Xiandi Pan <cindypan1807@gmail.com>
2023-11-16 20:13:27 -0500

    useState

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [name, setName] = useState('Cindy');
+  const [person, setPerson] = useState({name: 'mario', age: 40});
+
+  const clickHandler = () => {
+    setName("Xiandi");
+    setPerson({name: "lugi", age: 45})
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello World !! </Text>
-      </View>
-      <View style={styles.body}>
-        <Text> lorem ....</Text>
-        <Text> lorem ....</Text>
-        <Text> lorem... </Text>
-      </View>
+        <Text> my name is {name}</Text>
+        <Text>His name is {person.name} and his age is {person.age}</Text>
+        <View style={styles.buttonContainer}>
+          <Button title='update state' onPress={clickHandler}/>
+        </View>
+        <TextInput style={styles.input}></TextInput>
     </View>
   );
 }
@@ -23,14 +30,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-  }
+  buttonContainer: {
+  marginTop: 20,
+},
+TextInput: {
+  borderWidth: 1,
+  borderColor: 'black',
+  padding: 10,
+  margin: 10,
+}
 });
